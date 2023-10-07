58124538771f846488787e4597709b606845e5cc
Abdullah13130 <96126010+Abdullah13130@users.noreply.github.com>
2023-10-07 23:25:54 +0600

    create navbar,home , login page

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,16 @@
+import Slider from "../Slider/Slider";
+import Footer from "../sharedPage/Footer/Footer";
+import Navbar from "../sharedPage/Navbar/Navbar";
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <Slider></Slider>
+            <h3>thos is home</h3>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/pages/Slider/Slider.jsx
@@ -0,0 +1,11 @@
+
+const Slider = () => {
+    return (
+        <div>
+            <h4>this is sliderrrrr</h4>
+            {/* https://preview.themeforest.net/item/goeveni-event-sharing-social-network-html-template/full_screen_preview/25063662?_ga=2.110889734.288897255.1696611455-1675548590.1696611455 */}
+        </div>
+    );
+};
+
+export default Slider;
